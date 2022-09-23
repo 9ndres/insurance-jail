818f32a7a59470cf00b31f3dd625f845ed062470
andres <a.dev@mailfence.com>
2022-09-23 08:05:09 -0600

    added voucher_gen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 from docxtpl import DocxTemplate
 from pdfjinja import PdfJinja
+from datetime import date
 import batch_pdf
 
 doc = DocxTemplate('./templates/letters/Carta INS Remision MR.docx')
@@ -10,6 +11,18 @@ doc.render(project)
 doc.save('./templates/letters/new doc.docx')
 
 # batch_pdf.batch_convert()
-pdfjinja = PdfJinja('./templates/sample.pdf')
-pdfout = pdfjinja(dict(firstName='Spike'))
-pdfout.write(open('filled.pdf', 'wb'))
+current_date = date.today()
+slash_date = current_date.strftime("%d/%m/%Y")
+date_sl = dict(date_s=slash_date)
+
+
+def voucher_gen():
+    multiriesgo = PdfJinja('./templates/comprobante de entrega MR.pdf')
+    pdfout = multiriesgo(date_sl)
+    pdfout.write(open('comprobante multiriesgo.pdf', 'wb'))
+    riesgocivil = PdfJinja('./templates/comprobante de entrega RCG.pdf')
+    pdfout2 = riesgocivil(date_sl)
+    pdfout2.write(open('comprobante rcg.pdf', 'wb'))
+
+
+return
